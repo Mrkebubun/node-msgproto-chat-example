fe1c355cf69d18da85cb0e59a12bd56974a44506
Juan Batiz-Benet <juan@benet.ai>
2014-06-07 05:42:09 -0700

    got integrity working

---
+++ README.md
@@ -0,0 +1,3 @@
+# msgproto-chat-example
+
+An example of [msgproto](https://github.com/jbenet/node-ipfs-msgproto).
--- chat-protocol.js
+++ chat-protocol.js
@@ -1,21 +1,30 @@
 var WireProtocol = require('msgproto').WireProtocol
+var IntegrityProto = require('ipfs-msgproto-integrity')
 var ChatMessage = require('./chat-message')
 var transDuplex = require('duplex-transform')
+var shuffle = require('shuffle-buffer')
 
 module.exports = ChatProtocol
 
 function ChatProtocol(name, wire) {
-  var chats = WireProtocol(ChatMessage, wire)
-  return transDuplex.obj(encode, chats, decode)
+  wire = WireProtocol(IntegrityProto.Frame, wire)
+  wire = IntegrityProto.Corrupt(0.3, wire)
+  wire = IntegrityProto('sha1', ChatMessage, wire)
+  wire.incoming.on('invalid', function(obj) {
+    console.log('>> caught bad packet <<')
+  })
 
-  function encode(text, enc, next) {
+  wire = transDuplex.obj(chatEncode, wire, chatDecode) // the chat
+  return wire
+
+  function chatEncode(text, enc, next) {
     var now = (new Date()).toISOString()
     var msg = ChatMessage(name, text, now)
     this.push(msg)
     next()
   }
 
-  function decode(msg, enc, next) {
+  function chatDecode(msg, enc, next) {
     this.push(chatLine(msg))
     next()
   }
@@ -23,4 +32,5 @@ function ChatProtocol(name, wire) {
   function chatLine(msg) {
     return "[" + msg.time +"] " + msg.name + ": " + msg.text.trim() + "\n"
   }
+
 }
